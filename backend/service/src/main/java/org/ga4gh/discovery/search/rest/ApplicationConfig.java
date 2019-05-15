8898038afeeb1c0c0c0702d149d4d018caf1d124
Michal Linhard <michal.linhard@gmail.com>
2019-05-15 20:02:58 +0200

    SA-67: fix image build after removing sql-source module

--- backend/service/src/main/java/org/ga4gh/discovery/search/rest/ApplicationConfig.java
+++ backend/service/src/main/java/org/ga4gh/discovery/search/rest/ApplicationConfig.java
@@ -1,14 +1,9 @@
 package org.ga4gh.discovery.search.rest;
 
-import static com.google.common.base.Preconditions.checkArgument;
-
-import java.sql.SQLException;
-
 import org.ga4gh.discovery.search.serde.QueryDeserializer;
 import org.ga4gh.discovery.search.source.SearchSource;
 import org.ga4gh.discovery.search.source.presto.PrestoAdapterImpl;
 import org.ga4gh.discovery.search.source.presto.PrestoSearchSource;
-import org.ga4gh.discovery.search.source.sql.SQLSearchSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,26 +17,6 @@ import io.prestosql.sql.tree.Query;
 
 @Configuration
 public class ApplicationConfig {
-
-    /** MySQL */
-    @Value("${sql.datasource.enabled}")
-    private boolean sqlEnabled;
-
-    @Value("${sql.datasource.url}")
-    private String sqlDatasourceUrl;
-
-    @Value("${sql.datasource.username}")
-    private String sqlDatasourceUsername;
-
-    @Value("${sql.datasource.password}")
-    private String sqlDatasourcePassword;
-
-    @Value("${sql.datasource.driver-class-name}")
-    private String sqlDatasourceDriverClassName;
-
-    /** PrestoSQL */
-    @Value("${presto.datasource.enabled}")
-    private boolean prestoEnabled;
 
     @Value("${presto.datasource.url}")
     private String prestoDatasourceUrl;
@@ -60,23 +35,7 @@ public class ApplicationConfig {
     private String securityEnabled;
 
     @Bean
-    public SearchSource searchSource() throws ClassNotFoundException, SQLException {
-        checkArgument(
-                !(sqlEnabled && prestoEnabled), "SQL and Presto datasource can't be both enabled");
-        checkArgument(sqlEnabled || prestoEnabled, "At least one datasource has to be enabled");
-
-        return prestoEnabled ? getPrestoSearchSource() : getSqlSearchSource();
-    }
-
-    private SearchSource getSqlSearchSource() {
-        return new SQLSearchSource(
-                sqlDatasourceUrl,
-                sqlDatasourceUsername,
-                sqlDatasourcePassword,
-                sqlDatasourceDriverClassName);
-    }
-
-    private SearchSource getPrestoSearchSource() {
+    public SearchSource getPrestoSearchSource() {
         return new PrestoSearchSource(
                 new PrestoAdapterImpl(
                         prestoDatasourceUrl, prestoDatasourceUsername, prestoDatasourcePassword));
