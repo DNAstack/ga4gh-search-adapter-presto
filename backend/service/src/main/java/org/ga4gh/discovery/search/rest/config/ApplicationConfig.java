8be7ed88f4ef61c55dabb8c31f0db0f4553ad151
Fizz Ahmed <fizz@dnastack.com>
2019-08-26 10:50:14 -0400

    Removed dead datasetApiService code.

--- backend/service/src/main/java/org/ga4gh/discovery/search/rest/config/ApplicationConfig.java
+++ backend/service/src/main/java/org/ga4gh/discovery/search/rest/config/ApplicationConfig.java
@@ -5,6 +5,7 @@ import org.ga4gh.discovery.search.serde.QueryDeserializer;
 import org.ga4gh.discovery.search.source.SearchSource;
 import org.ga4gh.discovery.search.source.presto.PrestoAdapterImpl;
 import org.ga4gh.discovery.search.source.presto.PrestoSearchSource;
+import org.ga4gh.discovery.search.source.presto.SchemaService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -32,39 +33,6 @@ public class ApplicationConfig {
 
     @Value("${security.enabled:true}")
     private String securityEnabled;
-
-    /** Dataset Api Tools **/
-
-//    @Value("${dataset.page-size}")
-//    private int pageSize;
-//
-//    @Value("${dataset.api.force-scheme}")
-//    private String datasetApiForceScheme;
-//
-//    @Value("${dataset.api.url}")
-//    private String datasetApiUrl;
-//
-//    @Value("${dataset.api.schemas.ns}")
-//    private String datasetApiNs;
-
-//    private DatasetApiService datasetApiService;
-//
-//    private DatasetApiService createApiService() {
-//        DatasetApiService datasetApiService =
-//                //new DatasetApiService(datasetApiUrl, datasetApiNs, datasetApiForceScheme, pageSize);
-//                new DatasetApiService("http://localhost:8080/api", "ca.personalgenomes.schemas", null, 100);
-//        datasetApiService.initialize();
-//        return datasetApiService;
-//    }
-//
-//    @Bean
-//    public DatasetApiService getDatasetApiService() {
-//        // TODO FROM MICHAL: how to normally create a singleton that's also available in jsonObjectMapper() ?
-//        if (datasetApiService == null) {
-//            datasetApiService = createApiService();
-//        }
-//        return datasetApiService;
-//    }
 
     @Bean
     public SearchSource getPrestoSearchSource() {
