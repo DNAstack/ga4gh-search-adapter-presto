e33aa51ff3d65dadd2e6cb0f82a6ca12a17aab0a
Anshuman Banka <anshuman@dnastack.com>
2021-02-17 15:38:19 -0500

    enable prepost only on bearer auth

--- src/main/java/com/dnastack/ga4gh/search/adapter/security/GlobalMethodSecurityConfig.java
+++ src/main/java/com/dnastack/ga4gh/search/adapter/security/GlobalMethodSecurityConfig.java
@@ -1,11 +1,12 @@
 package com.dnastack.ga4gh.search.adapter.security;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @Configuration
-@Profile("!no-auth")
+@ConditionalOnExpression("'${app.auth.authorization-type}' == 'bearer'")
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class GlobalMethodSecurityConfig {
 }
